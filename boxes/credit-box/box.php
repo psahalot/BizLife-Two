3a451aa916288c26a4fab2b61fb6ea03b5ed774c
Puneet Sahalot <puneetsahalot@gmail.com>
2012-11-18 11:42:59 +0530

    Changed short tag to full PHP tag.

--- boxes/credit-box/box.php
+++ boxes/credit-box/box.php
@@ -17,5 +17,5 @@ $this->title = __('Credit', 'bizlife');
 
 public function html() { ?>
 &copy; Copyright <?php echo date('Y'); ?> | <a href="<?php bloginfo('url'); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a> | <a href="http://icustomizethesis.com">Thesis 2.0 Skin</a> by <a href="http://icustomizethesis.com">Puneet Sahalot</a>.
-   <?  }
+   <?php }
 }
